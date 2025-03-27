9423f8ea3aa584ccaa24a05dbb725af8c8175b56
junyoungccom <junyeongc1000@gmail.com>
2025-03-27 18:51:40 +0900

    버셀작업중

---
+++ app/account/guest/customer/edit/page.tsx
@@ -0,0 +1,4 @@
+export default function CustomerEditPage() {
+    return <div>고객 수정 페이지</div>;
+  }
+  
--- app/account/guest/customer/list/page.tsx
+++ app/account/guest/customer/list/page.tsx
@@ -1,1 +1,4 @@
-///
+export default function CustomerListPage() {
+    return <div>고객 조회 페이지</div>;
+  }
+  
